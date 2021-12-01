48084799496fc161b9e2f32602074ad2de3f5e55
Luiz Mitsuru Dai <61781452+Luiz326@users.noreply.github.com>
2021-12-01 12:15:00 -0300

    Create AppDbContext.cs

---
+++ api_rest/Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_rest.Persistence.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
