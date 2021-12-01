ca78014af2c948c879149cb7dfd03f6c47a8978b
Luiz Mitsuru Dai <61781452+Luiz326@users.noreply.github.com>
2021-12-01 11:09:48 -0300

    Initial commit

---
+++ api_rest/Domain/Model/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_rest.Domain.Model
+{
+    public class Class
+    {
+    }
+}
