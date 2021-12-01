3fbfc80057de54e26ac4dcf94d713ff324d790c6
Luiz Mitsuru Dai <61781452+Luiz326@users.noreply.github.com>
2021-12-01 18:01:12 -0300

    CategoryService atualizado

--- api_rest/Domain/Services/ICategoryService.cs
+++ api_rest/Domain/Services/ICategoryService.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api_rest.Domain.Models;
-
+using api_rest.Domain.Services.Communication;
 
 namespace api_rest.Domain.Services
 {
   public  interface ICategoryService
     {
         Task<IEnumerable<Category>> ListAsync();
+        Task<SaveCategoryResponse> SaveAsync(Category category);
     }
 }
