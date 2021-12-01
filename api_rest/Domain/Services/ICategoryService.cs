b90bfa23c01103e5dc6e0124da949fa5684629ca
Luiz Mitsuru Dai <61781452+Luiz326@users.noreply.github.com>
2021-12-01 11:36:18 -0300

    Services adicionado

---
+++ api_rest/Domain/Services/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api_rest.Domain.Model;
+
+
+namespace api_rest.Domain.Services
+{
+    interface ICategoryService
+    {
+        Task<IEnumerable<Category>> ListAsync();
+    }
+}
