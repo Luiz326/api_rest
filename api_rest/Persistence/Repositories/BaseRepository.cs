ce59794e7354dce1b0361cd0cad65fa138763d5c
Luiz Mitsuru Dai <61781452+Luiz326@users.noreply.github.com>
2021-12-01 14:12:02 -0300

    Criado BaseRepository

---
+++ api_rest/Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api_rest.Persistence.Contexts;
+
+namespace api_rest.Persistence.Repositories
+{
+    public abstract class BaseRepository
+    {
+        protected readonly AppDbContext _context;
+        public BaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
