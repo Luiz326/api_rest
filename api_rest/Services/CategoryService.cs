9be15dfa3df44955e6c3b552e55f85e967c9105c
Luiz Mitsuru Dai <61781452+Luiz326@users.noreply.github.com>
2021-12-01 11:58:38 -0300

    Create CategoryService.cs

---
+++ api_rest/Services/CategoryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api_rest.Domain.Models;
+using api_rest.Domain.Services;
+
+namespace api_rest.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        public async Task<IEnumerable<Category>> ListAsync()
+        {
+            return null;
+        }
+    }
+}
